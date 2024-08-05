e521acf66d5921f791a90e44fb11a21d65f01220
Kapekm <kacpermozdzen2002@gmail.com>
2024-08-05 18:40:36 +0200

    KM: Expand on tiles

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 20.0
-const JUMP_VELOCITY = -50.0
+const SPEED = 40.0
+const JUMP_VELOCITY = -150.0
 const gravity = 300
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
