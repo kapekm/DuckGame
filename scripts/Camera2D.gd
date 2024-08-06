231c68898edf0520003c48e6a2ae02417ce79d9a
Kapekm <kacpermozdzen2002@gmail.com>
2024-08-06 11:40:41 +0200

    KM: Progress on logic, tile change add killzone

---
+++ scripts/Camera2D.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+@onready var player = $"../Player"
+
+func _process(delta):
+	var position = player.global_position
+	if position.x > (global_position.x + 32):
+		global_position.x += 64
+		print(global_position.x)
+	elif position.x < (global_position.x - 32):
+		global_position.x -= 64
+
+		## Check if the player is outside the bounds on the y-axis
+	#if position.y > (global_position.y + 32):
+		#global_position.y += 64
+	#elif position.y < (global_position.y - 32):
+		#global_position.y -= 64
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 40.0
-const JUMP_VELOCITY = -150.0
+const JUMP_VELOCITY = -100.0
 const gravity = 300
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -11,7 +11,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -22,11 +21,11 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite_2d.play("idle")
+			animated_sprite_2d.play("run")
 		elif direction != 0:
-			animated_sprite_2d.play("idle")
+			animated_sprite_2d.play("run")
 	else:
-		animated_sprite_2d.play("idle")
+		animated_sprite_2d.play("run")
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -37,3 +36,4 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
