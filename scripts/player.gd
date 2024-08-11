04393943320f6cf6611f50c2f0827e24369fc873
Kapekm <kacpermozdzen2002@gmail.com>
2024-08-11 08:54:14 +0200

    KM: add double jump and wall jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,43 +7,20 @@ const ACCEL = 300
 const DECCEL = 300
 const GRAVITY = 300
 
-var is_on_wall = false
-var wall_dir = 0
+var jump_count = 0
+var max_jump = 2
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var collision_shape_2d = $CollisionShape2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-	
-	#if is_on_wall():
-		#var wall_dir = sign(get_wall_normal().x)
-	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		elif is_on_wall:
-			velocity.y = JUMP_VELOCITY
-			velocity.x = -wall_dir * WALL_JUMP_VELOCITY_X
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("idle")
-		elif direction != 0:
-			animated_sprite_2d.play("walk")
-	else:
-		animated_sprite_2d.play("walk")
-	
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCEL * delta)
-		print(velocity.x)
+		
 		#if direction is lower than 0 flip to other side
 		if direction < 0:
 			animated_sprite_2d.offset.x = 7
@@ -53,9 +30,36 @@ func _physics_process(delta):
 			animated_sprite_2d.offset.x = 0
 			animated_sprite_2d.flip_h = false
 			
-		
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECCEL * delta)
-
+	
+	if is_on_floor():
+		jump_count = 0
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		elif direction != 0:
+			animated_sprite_2d.play("walk")
+	else: #they are jumping
+		animated_sprite_2d.play("walk")
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	
+	if is_on_wall() and Input.is_action_just_pressed("jump"):
+		print(velocity.x)
+		if velocity.x > 0:
+			velocity = Vector2(-70, JUMP_VELOCITY)
+		elif velocity.x < 0:
+			velocity = Vector2(70, JUMP_VELOCITY)
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor() and jump_count < max_jump:
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+	if Input.is_action_just_pressed("jump") and !is_on_floor() and jump_count < max_jump and !is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+	
 	move_and_slide()
 	
