a50dbe9566ae8cc3899ba7a73d691755dab48de4
Kapekm <kacpermozdzen2002@gmail.com>
2024-08-11 09:16:39 +0200

    KM: Add wall_collider raycast

--- scripts/player.gd
+++ scripts/player.gd
@@ -35,6 +35,7 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		jump_count = 0
+		
 		if direction == 0:
 			animated_sprite_2d.play("idle")
 		elif direction != 0:
@@ -43,8 +44,10 @@ func _physics_process(delta):
 		animated_sprite_2d.play("walk")
 	
 	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_wall():
 		velocity.y += GRAVITY * delta
+	elif is_on_wall():
+		velocity.y += GRAVITY * 0.1 * delta
 	
 	if is_on_wall() and Input.is_action_just_pressed("jump"):
 		print(velocity.x)
